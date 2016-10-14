72f1a867a304404fe3ac42eb474a49256fa7c7f6
ruiwei <liangruiwei777@gmail.com>
2016-10-14 11:11:07 +1100

    css changes

--- static/scripts/navbar-bg.js
+++ static/scripts/navbar-bg.js
@@ -1,7 +1,7 @@
 /**
  * Created by user on 17/08/16.
  */
-$(".navbar").css("backgroundColor", "#001b31").css("padding", "10px 0 15px")
+$(".navbar").css("backgroundColor", "#001b31").css("padding", "0")
 
 
 
